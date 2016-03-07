673486ceda683643b34a0f4c5acf6271cb5e6c99
Joachim De Beule <joachim.de.beule@gmail.com>
2016-03-07 11:52:50 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject atomix-test "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[cider/cider-nrepl "0.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [io.atomix/atomix-all "1.0.0-rc2"]])
